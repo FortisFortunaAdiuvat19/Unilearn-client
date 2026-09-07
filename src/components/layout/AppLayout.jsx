62fda1de651a44da974886fc9c7c2e6adceacf54
Fortune <extremeforx@gmail.com>
2026-09-07 16:54:30 +0100

    Add BackButton component to AppLayout

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import BackButton from "./BackButton";
 
 export default function AppLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
+      <BackButton />
       <main className="flex-1">
         <Outlet />
       </main>
